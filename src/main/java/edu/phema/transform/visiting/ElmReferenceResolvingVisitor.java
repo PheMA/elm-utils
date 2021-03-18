1e8be49de1128c657700e3e82834e6f9e1c24ab2
Pascal Brandt <psbrandt@gmail.com>
2021-03-17 20:52:07 -0700

    🐫 Fix null issue with external references.

--- src/main/java/edu/phema/transform/visiting/ElmReferenceResolvingVisitor.java
+++ src/main/java/edu/phema/transform/visiting/ElmReferenceResolvingVisitor.java
@@ -8,38 +8,41 @@ import org.hl7.elm.r1.ExpressionRef;
 import java.util.Optional;
 
 public class ElmReferenceResolvingVisitor extends ElmBaseStatementPostOrderTransformationVisitor<Void, ElmReferenceResolvingContext> {
-    public ElmReferenceResolvingVisitor() {
-        super(false);
+  public ElmReferenceResolvingVisitor() {
+    super(false);
+  }
+
+  public ElmReferenceResolvingVisitor(boolean debug) {
+    super(debug);
+  }
+
+  private Expression resolveReference(ExpressionRef expressionRef, ElmReferenceResolvingContext context) {
+    Optional<ExpressionDef> referencedExpressionDef = context
+      .getLibrary()
+      .getStatements()
+      .getDef()
+      .stream()
+      .filter(ed -> ed.getName().equals(expressionRef.getName()))
+      .findFirst();
+
+    // This just returns the Ref if the actual expression isn't found
+    // Need to figure out a way to check included libraries
+    Expression target = referencedExpressionDef.map(ExpressionDef::getExpression).orElse(expressionRef);
+
+    // FIXME: Figure out how to fail
+    context.addMap(target);
+
+    return target;
+  }
+
+  @Override
+  public Void visitExpression(Expression elm, ElmReferenceResolvingContext context) {
+    if (elm instanceof ExpressionRef) {
+      Expression target = this.resolveReference((ExpressionRef) elm, context);
+
+      return super.visitExpression(target, context);
     }
 
-    public ElmReferenceResolvingVisitor(boolean debug) {
-        super(debug);
-    }
-
-    private ExpressionDef resolveReference(ExpressionRef expressionRef, ElmReferenceResolvingContext context) {
-        Optional<ExpressionDef> referencedExpressionDef = context
-            .getLibrary()
-            .getStatements()
-            .getDef()
-            .stream()
-            .filter(ed -> ed.getName().equals(expressionRef.getName()))
-            .findFirst();
-
-        Expression target = referencedExpressionDef.map(ExpressionDef::getExpression).orElse(null);
-
-        // FIXME: Figure out how to fail
-        context.addMap(target);
-
-        return referencedExpressionDef.orElse(null);
-    }
-
-    @Override
-    public Void visitExpression(Expression elm, ElmReferenceResolvingContext context) {
-        if (elm instanceof ExpressionRef) {
-            ExpressionDef target = this.resolveReference((ExpressionRef) elm, context);
-            return super.visitExpressionDef(target, context);
-        }
-
-        return super.visitExpression(elm, context);
-    }
+    return super.visitExpression(elm, context);
+  }
 }
